204c634e4332ab91a0a61c1287b5599d0d8f106a
Vladyslav Arseniuk <vlad.arsenyuk@gmail.com>
2026-07-16 20:50:04 +0300

    Project init with git ignore

---
+++ superSecureNotes/superSecureNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  superSecureNotesApp.swift
+//  superSecureNotes
+//
+//  Created by Vladyslav Arseniuk on 7/16/26.
+//
+
+import SwiftUI
+
+@main
+struct superSecureNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
